a5442ec3d716ea7518ed0b7be644dd3ca60cd620
Gyunni <mingyun12304@gmail.com>
2021-08-19 19:18:10 +0900

    사칙연산

---
+++ BOJ/10869.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var input = readLine()!!.split(' ')
+    var a = input[0].toInt()
+    var b = input[1].toInt()
+    println(a+b)
+    println(a-b)
+    println(a*b)
+    println(a/b)
+    println(a%b)
+}
