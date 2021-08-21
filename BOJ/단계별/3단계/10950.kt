8aa33715dc2a139195c159368cb4ecec652b8f03
Gyunni <mingyun12304@gmail.com>
2021-08-21 14:21:07 +0900

    A+B - 3

---
+++ BOJ/단계별/3단계/10950.kt
@@ -0,0 +1,13 @@
+import java.util.*
+fun main(){
+    val sc = Scanner(System.`in`)
+    var t = sc.nextInt()
+    
+    while(t>0){
+        var a = sc.nextInt()
+        var b = sc.nextInt()
+        
+        println(a+b)
+        t = t-1
+    }
+}
