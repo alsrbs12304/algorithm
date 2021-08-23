5c131ef716664b101b5af946b3bd2b1dafdf2819
Gyunni <mingyun12304@gmail.com>
2021-08-23 21:10:05 +0900

    별 찍기 - 1

---
+++ BOJ/단계별/3단계/2438.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main(){
+    val sc = Scanner(System.`in`)
+    val n = sc.nextInt()
+    
+    for(i in 1..n){
+        for(j in 1..i){
+            print("*")
+        }
+        println("")
+    }
+}
